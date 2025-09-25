99c65fcf2f304a64e101fbba3a6a74dcf144c170
codeKarma1 <eawoleke@gmail.com>
2025-09-25 11:29:43 +0100

    first commit

---
+++ todo-app/src/components/TodoItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import tick from "../assets/tick.png";
+import nottick from "../assets/not_tick.png";
+import deleteicon from "../assets/delete.png";
+
+const TodoItem = ({ text, id, isComplete, deleteTodo, toggle }) => {
+  return (
+    <div className="flex items-center my-3 gap-2">
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex flex-1
+      items-center cursor-pointer"
+      >
+        <img src={isComplete ? tick : nottick} alt="" className="w-7" />
+        <p
+          className={`ml-4 text-[17px] ${
+            isComplete ? "line-through text-slate-500" : "text-slate-700"
+          }`}
+        >
+          {text}
+        </p>
+      </div>
+
+      <img
+        onClick={() => {
+          deleteTodo(id);
+        }}
+        src={deleteicon}
+        alt=""
+        className="w-3.5 cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default TodoItem;
